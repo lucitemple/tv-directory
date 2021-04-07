518fd1282ae062532a25fec8bee316da6bea0705
Luci Temple <lucitemple@yahoo.com.au>
2021-04-07 18:24:11 +1000

    Add loader spinner font awesome

---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import loaderSrc from "../../assets/loader.png";
+
+const Loader = (props) => (
+  <div>
+    <i 
+    className="fas fa-spinner fa-pulse fa-3x"></i>
+  </div>
+);
+
+export default Loader;
