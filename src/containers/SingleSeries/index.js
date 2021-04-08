d527e5a101267074313ba0aeb7d7ac9202b6a369
Luci Temple <lucitemple@yahoo.com.au>
2021-04-08 17:59:14 +1000

    Change API to https

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -8,7 +8,7 @@ class SingleSeries extends Component {
 
   componentDidMount() {
     const { id } = this.props.match.params;
-    fetch(`http://api.tvmaze.com/shows/${id}?episodes`)
+    fetch(`https://api.tvmaze.com/shows/${id}?episodes`)
       .then((response) => response.json())
       .then((json) => this.setState({ show: json }));
   }
